9cc9a596e74d8a4a5d46cfaa032bd86b0c4db3cb
aaneja <aaneja@ebi.ac.uk>
2022-04-01 13:20:37 +0530

    some more env vars update

--- src/ensembl/production/metadata/config.py
+++ src/ensembl/production/metadata/config.py
@@ -5,12 +5,12 @@ class MetadataConfig:
     metadata_host = os.environ.get("METADATA_DB_HOST")
     metadata_port = os.environ.get("METADATA_DB_PORT")
     metadata_user = os.environ.get("METADATA_DB_USER")
-    metadata_pass = os.environ.get("METADATA_DB_PASSWORD")
+    metadata_pass = os.environ.get("METADATA_DB_PASS")
 
     taxon_host = os.environ.get("TAXON_DB_HOST")
     taxon_port = os.environ.get("TAXON_DB_PORT")
     taxon_user = os.environ.get("TAXON_DB_USER")
-    taxon_pass = os.environ.get("TAXON_DB_PASSWORD")
+    taxon_pass = os.environ.get("TAXON_DB_PASS")
 
     pool_size = os.environ.get("POOL_SIZE", 20)
     max_overflow = os.environ.get("MAX_OVERFLOW", 0)
