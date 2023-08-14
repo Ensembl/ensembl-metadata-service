0a66fb43e01bae57b64cf7130bd90989ed08829c
Sanjay Boddu <sboddu@ebi.ac.uk>
2023-08-14 17:27:31 +0100

    Narrow down the test

--- src/tests/test_service.py
+++ src/tests/test_service.py
@@ -489,6 +489,17 @@ class TestClass:
         }
         assert json.loads(output) == expected_output
 
+    def test_get_genome_uuid_assembly_default(self, engine):
+        output = json_format.MessageToJson(
+            service.get_genome_uuid(
+                engine,
+                "homo_sapiens", "GRCh37", True))
+        expected_output = {
+            "genomeUuid": "3704ceb1-948d-11ec-a39d-005056b38ce3"
+        }
+        assert json.loads(output) == expected_output
+
+
     def test_get_genome_by_uuid(self, engine):
         output = json_format.MessageToJson(
             service.get_genome_by_uuid(engine,
