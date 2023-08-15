f9d47dd9d894418bad72dc5f70dad071ac9ae45d
Sanjay Boddu <sboddu@ebi.ac.uk>
2023-08-15 16:02:35 +0100

    Test travis with False

--- src/tests/test_service.py
+++ src/tests/test_service.py
@@ -507,8 +507,7 @@ class TestClass:
         expected_output = {
             "genomeUuid": "3704ceb1-948d-11ec-a39d-005056b38ce3"
         }
-
-        assert json.loads(output) == {}
+        assert json.loads(output) == expected_output
 
     def test_get_genome_by_uuid(self, engine):
         output = json_format.MessageToJson(
